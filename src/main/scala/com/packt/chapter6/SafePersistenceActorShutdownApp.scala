156ab33f12e181a46c27ca1de95e4ca283619630
msomasundaram <maheshsoms@csu.fullerton.edu>
2019-06-12 20:58:24 -0700

    new changes

--- src/main/scala/com/packt/chapter6/SafePersistenceActorShutdownApp.scala
+++ src/main/scala/com/packt/chapter6/SafePersistenceActorShutdownApp.scala
@@ -5,8 +5,8 @@ import akka.actor.{ActorSystem, PoisonPill, Props}
 object SafePersistenceActorShutdownApp extends App{
   val system=ActorSystem("safe-shutdown")
 
-  val persistentActor1=system.actorOf(Props[SamplePersistentActor])
-  val persistentActor2=system.actorOf(Props[SamplePersistentActor])
+  val persistentActor1=system.actorOf(Props[SamplePersistenceActor])
+  val persistentActor2=system.actorOf(Props[SamplePersistenceActor])
 
 
   persistentActor1 ! UserUpdate("Naai",Add)
